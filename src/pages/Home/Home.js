48cdcf2a63b3957b78a8d2fbc9f935b229d85c7b
Elijah Xiong <exiong@icstars.org>
2021-03-26 15:52:11 -0500

    intro to react

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="root">
+      <h1>Home Page!!!!!!!!</h1>
+      <p>lorem ipsum</p>
+      <h2>more</h2>
+      <button>login</button>
+    </div>
+  );
+}
+
+export default Home;
